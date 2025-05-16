ac7d01a8e185643791082105b4f339e3e729e8e3
ilovecode222 <wschreibe@conncoll.edu>
2025-05-15 20:21:15 -0400

    update databas

--- db/migrate/20250512000000_create_events.rb
+++ db/migrate/20250512000000_create_events.rb
@@ -1,10 +1,5 @@
 class CreateEvents < ActiveRecord::Migration[7.2]
   def change
-    create_table :events do |t|
-      t.string :name
-      t.string :category
 
-      t.timestamps
-    end
   end
 end
--- db/migrate/20250512221323_add_school_and_grade_to_athletes.rb
+++ db/migrate/20250512221323_add_school_and_grade_to_athletes.rb
@@ -1,6 +1,4 @@
 class AddSchoolAndGradeToAthletes < ActiveRecord::Migration[7.2]
   def change
-    add_column :athletes, :school, :string
-    add_column :athletes, :grade, :date
   end
 end
