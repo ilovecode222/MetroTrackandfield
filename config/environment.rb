64e0abfc9f9af82fc453293209bb6fcd66bfa692
ilovecode222 <wschreibe@conncoll.edu>
2025-05-15 20:59:28 -0400

    Your descriptive commit message here

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require_relative "application"
 
 # Debug RAILS_MASTER_KEY presence before Rails initializes
 puts "RAILS_MASTER_KEY: #{ENV['RAILS_MASTER_KEY'].present? ? 'Present' : 'Missing'}"
+puts "PORT: #{ENV['PORT'] || 'not set'}"
 
 # Initialize the Rails application.
 Rails.application.initialize!
